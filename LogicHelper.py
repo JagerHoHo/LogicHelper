30422aa63126193e6717c0770b4f512a47bf3da5
Jager Liu <hohoj825@gmail.com>
2021-02-03 08:51:37 +0800

    a part

--- LogicHelper.py
+++ LogicHelper.py
@@ -1,3 +1,66 @@
+def a_part():
+    print("Do not input the So in the conclusion. Words like all, no, some, not, is, are should be in lowercase.")
+    print("If they appear more than one time, only the one with logical usefulness shuold be in lowercase")
+    print("make the one without logical usefulness fully capital like all cat are things that ARE cute")
+    argument = []
+    arg_part = ["premise 1", "premise 2", "conclusion"]
+    arg_type = []
+    temp_counter = 0
+
+    while len(argument) < 3:
+        argument.append(
+            input("Input your " + arg_part[temp_counter] + ": ").strip().split())
+        if argument[temp_counter][0] in ["all", "no", "some"] and ("is" in argument[temp_counter] or "are" in argument[temp_counter]):
+            arg_type.append(typefinder(argument, temp_counter))
+            temp_counter += 1
+        else:
+            print("Invalid input found in your " + arg_part[temp_counter])
+            argument.pop(temp_counter)
+
+    s, p = findterms(2, arg_type, argument)
+    p1_first, p1_sec = findterms(0, arg_type, argument)
+    p2 = [*findterms(1, arg_type, argument)]
+    try:
+        m = {p1_first, p1_sec}.intersection(p2).pop()
+    except:
+        print("No M has been found.")
+        return
+    m_is_before_verb = [True, True]
+
+    if p in argument[0]:
+        p1_position, p2_position = 0, 1
+    else:
+        p1_position, p2_position = 1, 0
+        arg_type[0], arg_type[1] = arg_type[1], arg_type[0]
+
+    m_is_before_verb[p1_position] = True if p1_first == m else False
+    m_is_before_verb[p2_position] = True if p2[0] == m else False
+
+    Figure = {
+        (True, True): 3,
+        (False, False): 2,
+        (True, False): 1,
+        (False, True): 4
+    }
+
+    figure = str(Figure[tuple(m_is_before_verb)])
+
+    standard_form = ''.join(
+        arg_type[0][0] + arg_type[1][0] + arg_type[2][0] + figure)
+    validity = "Invalid, the Venn diagram does not contain all the information of the conclusion."
+    valid_set = [
+        "AAA1", "EAE1", "AEE2", "EAE2", "AEE4", "AII1", "AII3", "IAI3",
+        "IAI4", "EIO1", "AOO2", "EIO2", "EIO3", "OAO3", "EIO3"
+    ]
+    if standard_form in valid_set:
+        validity = "Valid, the Venn diagram contains all the information of the conclusion."
+    print("standard_form: " + standard_form)
+    print("P = " + p)
+    print("M = " + m)
+    print("S = " + s)
+    print(validity)
+
+
 def c_part():
     existential_import = input("With Existential Import? (YES/NO): ").upper()
     if existential_import in ["YES", "NO"]:
@@ -26,7 +89,7 @@ def d_part():
         print(sentence)
 
 
-def typefinder():
+def typefinder(argument, temp_counter):
     if "all" in argument[temp_counter]:
         return ['A', "is"] if "is" in argument[temp_counter] else ['A', "are"]
     if "no" in argument[temp_counter]:
@@ -37,7 +100,7 @@ def typefinder():
         return ['I', "is"] if "is" in argument[temp_counter] else ['I', "are"]
 
 
-def findterms(index):
+def findterms(index, arg_type, argument):
     verb = arg_type[index][1]
     start_of_sec = argument[index].index(verb) + 1
     sec = " ".join(argument[index][start_of_sec:])
@@ -140,77 +203,7 @@ while not exit:
     q_type = input("I am answering: ").upper().strip()
 
     if q_type == "A":
-        print(
-            "Do not input the So in the conclusion. Words like all, no, some, not, is, are should be in lowercase."
-        )
-        print(
-            "If they appear more than one time, only the one with logical usefulness shuold be in lowercase"
-        )
-        print(
-            "make the one without logical usefulness fully capital like all cat are things that ARE cute"
-        )
-        argument = []
-        arg_part = ["premise 1", "premise 2", "conclusion"]
-        arg_type = []
-        temp_counter = 0
-
-        while len(argument) < 3:
-            argument.append(
-                input("Input your " + arg_part[temp_counter] +
-                      ": ").strip().split())
-            if argument[temp_counter][0] in [
-                    "all", "no", "some"
-            ] and ("is" in argument[temp_counter]
-                   or "are" in argument[temp_counter]):
-                arg_type.append(typefinder())
-                temp_counter += 1
-            else:
-                print("Invalid input found in your " + arg_part[temp_counter])
-                argument.pop(temp_counter)
-
-        s, p = findterms(2)
-        p1_first, p1_sec = findterms(0)
-        p2 = [*findterms(1)]
-        try:
-            m = {p1_first, p1_sec}.intersection(p2).pop()
-        except:
-            print("No M has been found.")
-            break
-        m_is_before_verb = [True, True]
-
-        if p in argument[0]:
-            p1_position, p2_position = 0, 1
-        else:
-            p1_position, p2_position = 1, 0
-            arg_type[0], arg_type[1] = arg_type[1], arg_type[0]
-
-        m_is_before_verb[p1_position] = True if p1_first == m else False
-        m_is_before_verb[p2_position] = True if p2[0] == m else False
-
-        Figure = {
-            (True, True): 3,
-            (False, False): 2,
-            (True, False): 1,
-            (False, True): 4
-        }
-
-        figure = str(Figure[tuple(m_is_before_verb)])
-
-        standard_form = ''.join(arg_type[0][0] + arg_type[1][0] +
-                                arg_type[2][0] + figure)
-        validity = "Invalid, the Venn diagram does not contain all the information of the conclusion."
-        valid_set = [
-            "AAA1", "EAE1", "AEE2", "EAE2", "AEE4", "AII1", "AII3", "IAI3",
-            "IAI4", "EIO1", "AOO2", "EIO2", "EIO3", "OAO3", "EIO3"
-        ]
-        if standard_form in valid_set:
-            validity = "Valid, the Venn diagram contains all the information of the conclusion."
-        print("standard_form: " + standard_form)
-        print("P = " + p)
-        print("M = " + m)
-        print("S = " + s)
-        print(validity)
-
+        a_part()
     elif q_type == "B":
         print("隨緣更新")
     elif q_type == "C":
